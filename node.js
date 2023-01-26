f033a352673ce378fcbfefd3db1c336ec7ec7147
skstanwar <kumarsaini0098@gmail.com>
2023-01-26 21:51:38 +0530

    second update

--- node.js
+++ node.js
@@ -1,12 +1,35 @@
-const {MongoClient} = require('mongodb');
-const url='mongodb://0.0.0.0:27017';
-const client = new MongoClient(url);
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://0.0.0.0:27017";
 
-async function getd(){
-  const result=  await client.connect();
-const rr= await result.db('admin');
-const cc= rr.collection("saini");
-const final= await cc.find({}).toArray();
-console.log(final);
+MongoClient.connect(url , function(err , db){
+  if(err) throw err;
+  console.log("Connection is done!!");
+const dbo= db.db('admin');
+dbo.createCollection('NewC' , function(err, res){
+  if(err) throw err;
+  console.log("New Collection is created !!");
+
+})
+var obj ={
+
+  Year:
+  "1991",
+  Industry_aggregation_NZSIOC:
+  "Level_N",
+  Industry_code_NZSIOC:
+  "1111",
+  Industry_name_NZSIOC:
+  "STAR BUILDING MAINTAINANCE LLC"
+  
+
 }
-getd();
+dbo.collection('test_file').insertOne(obj , function(err, res){
+  if(err) throw err;
+  console.log("data Inserted");
+})
+
+
+
+db.close();
+
+})
